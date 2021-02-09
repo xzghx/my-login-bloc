1e1fd629b87448afacdf1200448bc127dfacb640
Zahra <ghafoory.zahra@gmail.com>
2021-02-09 14:47:37 +0330

    added bloc observer

---
+++ lib/simple_bloc_observer.dart
@@ -0,0 +1,17 @@
+import 'package:app_login_signin/auth_bloc/auth_exports.dart';
+import 'package:app_login_signin/login_bloc/login_bloc_exports.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onChange(Cubit cubit, Change change) {
+    if (cubit is LoginCubit) print((cubit as LoginCubit).state.formzStatus.toString());
+    super.onChange(cubit, change);
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    if (bloc is AuthBloc) print((bloc as AuthState).status.toString());
+    super.onEvent(bloc, event);
+  }
+}
